eeb7a7d7e4657d42c8a7e058178bf175247500a8
anjlnk <ibrm632@gmail.com>
2023-03-09 23:56:54 +0700

    Initial Commit

---
+++ my_proj/todo_app/admin.py
@@ -0,0 +1,8 @@
+from atexit import register
+from django.contrib import admin
+from .models import ToDoItem, ToDoList
+
+# Register your models here.
+admin.site.register(ToDoItem)
+admin.site.register(ToDoList)
+
